fb7d0689f5be14bbc4367534cd5bf32c8a967d7a
armyenokyan <armyenokyan@gmail.com>
2021-09-26 13:52:07 +0400

    “update260921-final”

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,13 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="footer">
-      <p>All rights reserved</p>
+    <footer className="footer ">
+      <p>
+        <b>GitHub: </b>
+        <a target="_blank" rel="noreferrer" href="https://github.com/armyenokyan/covid19/">
+          https://github.com/armyenokyan/covid19
+        </a>
+      </p>
     </footer>
   );
 };
